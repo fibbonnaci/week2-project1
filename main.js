52b986a2e2c7e5f28418ae64ef3f2b9a67a8311b
fibbonnaci <jyothinookula@gmail.com>
2016-07-26 20:05:39 -0700

    question 1-9

--- main.js
+++ main.js
@@ -27,6 +27,13 @@ ADVANCED TRACK: 14-20
 */
 
 // 1. Create an object called "me" that describes you with your name, city, and job title.
+console.log("Question 1");
+var me = {
+	name: 'Jyothi',
+	city: 'Seattle',
+	jobTitle: 'Product Manager'
+};
+console.log(me);
 
 // 2. To your "me" object, add a key of "family" with the value being an array of three of your family members.
 // Example of desired output: { name: 'Bob Smith',
@@ -34,7 +41,25 @@ ADVANCED TRACK: 14-20
 //                              jobTitle: 'professional wrestler',
 //                              family: ['Joan', 'Kyle', 'Hank'] }
 
+
+console.log("Question 2");
+var me = {
+	name: 'Jyothi',
+	city: 'Seattle',
+	jobTitle: 'Product Manager'
+};
+me.family= ['dad','mom','granny'];
+console.log(me);
 // 3. Using the "me" object, use dot notation and bracket notation to access the value of your city.
+
+console.log("Question 3");
+var me = {
+	name: 'Jyothi',
+	city: 'Seattle',
+	jobTitle: 'Product Manager'
+};
+me.family= ['dad','mom','granny'];
+console.log(me.city);
 
 // Use the following Object for questions 4-9:
 var invitees = { adults: [{
@@ -72,18 +97,35 @@ var invitees = { adults: [{
                       };
 
 // 4. Access Penny's dinner choice (this will be the second item in his dinnerChoice array)
+console.log("Question 4");
+console.log(invitees.adults[0].dinnerChoice);   
 
 // 5. Access Pablo's table number.
 
+console.log("Question 5");
+console.log(invitees.children[1].table);  
+
 // 6. Access Lauren's seating specialConsideration.
+
+console.log("Question 6");
+console.log(invitees.adults[2].specialConsiderations);    
 
 // 7. Access Billy's dessert choice (this will be the third item in his dinnerChoice array)
 
+console.log("Question 7");
+console.log(invitees.adults[1].dinnerChoice[2]);         
+
 // 8. Access Ada's allergies.
+
+console.log("Question 8");
+console.log(invitees.children[2].allergies); 
 
 // 9. Create a string that reads Lauren's dinner choice in a sentence.
 // Desired result: "Lauren's appetizer is _______. She will be eating ______ for dinner
 // and has selected ________ as her dessert."
+
+console.log("Question 9");
+console.log("Lauren's appetizer is "+ (invitees.adults[2].dinnerChoice[0])+". She will be eating "+(invitees.adults[2].dinnerChoice[1])+ " and has selected "+ (invitees.adults[2].dinnerChoice[2])+ " as her dessert");
 
 // 10. Create an empty object called "jsWorkshop" using Constructor Notation.
 
